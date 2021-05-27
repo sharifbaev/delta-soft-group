b108cfb4e4429fa0d285f31fa7a38e247f1781ec
Aziz <example@gmail.com>
2021-05-27 17:00:16 +0500

    create repository

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		slidesToShow:3,
+	});
+});
+
+$(document).ready(function(){
+	$('.slider_2').slick({
+		arrows:true,
+		slidesToShow:4,
+	});
+});
